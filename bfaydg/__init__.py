58578045960d53346e2c9622f63d186608f8dd3c
Axel Yael Díaz García <di476809@uaeh.edu.mx>
2026-02-27 00:08:21 +0000

    funciones nuevas

---
+++ bfaydg/__init__.py
@@ -0,0 +1,33 @@
+from typing_extensions import Sequence
+def suma_de_valores(values, keys):
+  # Initializes the sum to 0
+  sum=0
+  # Gets the number of elements in the 'values' list
+  n=len(values)
+  # Iterates through the elements and calculates the weighted sum
+  for i in range(n):
+    sum+=values[i]*keys[i]
+  # Returns the calculated sum
+  return sum
+#Capacidad de la mochila
+def prblema_mochila(capacity, goal, weights, profits):
+  # capacity is the maximum weight the knapsack can hold
+  # weights is a list representing the weights of the items
+  # profits is a list representing the profits of the items
+  # goal is the minimum profit required
+
+  n=len(weights)
+  sequence=itt.product([0, 1], repeat=n)
+  for k in sequence:
+    if suma_de_valores(weights, k) <= capacity:
+      if suma_de_valores(profits, k) >= goal:
+        return k
+  return False
+beneficio=[8, 5, 7, 5, 10, 8, 11, 13, 5, 11]
+peso=[10, 8, 3, 4, 11, 7, 13, 10, 8, 9]
+capacidad=60
+meta=70
+print(prblema_mochila(capacidad,
+                      meta,
+                      peso,
+                      beneficio))
